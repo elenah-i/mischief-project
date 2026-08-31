409aba764a52416a4e0115c6e80561d86389a027
Aleena Ishaq <elinahishaq@gmail.com>
2026-08-31 15:25:30 +0500

    Add logging settings to Django configuration
    Added logging configuration for Django application.

--- backend/mischief/settings.py
+++ backend/mischief/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGGING = { 'version': 1, 'disable_existing_loggers': False, 'handlers': { 'console': { 'class': 'logging.StreamHandler', }, }, 'root': { 'handlers': ['console'], 'level': 'WARNING', }, 'loggers': { 'django': { 'handlers': ['console'], 'level': 'INFO', 'propagate': False, }, 'django.request': { 'handlers': ['console'], 'level': 'ERROR', 'propagate': False, }, }, }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
